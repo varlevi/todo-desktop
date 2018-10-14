b1ae565da9e88a1fd7d4cd345ee5f4a959a69e18
Ian Fisher <iafisher@protonmail.com>
2018-10-14 11:03:10 -0400

    Make Electron behave properly on macOS

--- electron-prototype/main.js
+++ electron-prototype/main.js
@@ -1,8 +1,32 @@
 const { app, BrowserWindow } = require("electron");
 
+// Global reference to window object.
+let win;
+
 function createWindow() {
-    let win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({ width: 800, height: 600 });
     win.loadFile("index.html");
+
+    win.webContents.openDevTools();
+
+    win.on("closed", () => {
+        win = null;
+    });
 }
 
 app.on("ready", createWindow);
+
+// These functions are from the tutorial:
+//   https://electronjs.org/docs/tutorial/first-app
+// They make the application behavior properly on macOS.
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+});
+
+app.on("activate", () => {
+    if (win === null) {
+        createWindow();
+    }
+});
