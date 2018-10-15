d52581c4211665c214cf1ba55fa1b55f91853770
Ian Fisher <iafisher@protonmail.com>
2018-10-14 20:13:05 -0400

    Write unit tests for (de)serialization

---
+++ test/test_serde.js
@@ -0,0 +1,196 @@
+const assert = require("assert");
+const serde = require("../serde.js");
+
+
+describe("serialize", function () {
+    it("should return empty string for no data", function () {
+        assert.strictEqual(serde.serialize([]), "");
+    });
+
+    it("should handle section with no items", function () {
+        const data = [
+            { title: "Thursday", items: [] },
+        ];
+        assert.strictEqual(serde.serialize(data), "=== Thursday ===\n\n");
+    });
+
+    it("should handle one section with some items", function () {
+        const data = [
+            {
+                title: "Monday",
+                items: [
+                    { text: "Study for CS exam", finished: false },
+                    { text: "Philosophy paper", finished: true },
+                ]
+            },
+        ];
+        assert.strictEqual(
+            serde.serialize(data),
+            "=== Monday ===\n- Study for CS exam\n- ~Philosophy paper~\n\n"
+        );
+    });
+
+    it("should handle multiple sections, all with items", function () {
+        const data = [
+            {
+                title: "Work",
+                items: [
+                    { text: "Submit reimbursement form", finished: false },
+                    { text: "Prep for 1:1", finished: true },
+                ]
+            },
+            {
+                title: "Home",
+                items: [
+                    { text: "Buy milk", finished: true },
+                    { text: "Fix lawnmower", finished: true },
+                ]
+            },
+        ];
+        assert.strictEqual(
+            serde.serialize(data),
+            "=== Work ===\n- Submit reimbursement form\n- ~Prep for 1:1~\n\n" +
+            "=== Home ===\n- ~Buy milk~\n- ~Fix lawnmower~\n\n"
+        );
+    });
+
+    it("should handle multiple sections, some blank", function () {
+        const data = [
+            {
+                title: "Work",
+                items: [
+                    { text: "Submit reimbursement form", finished: false },
+                    { text: "Prep for 1:1", finished: true },
+                ]
+            },
+            {
+                title: "School",
+                items: []
+            },
+            {
+                title: "Home",
+                items: [
+                    { text: "Buy milk", finished: true },
+                    { text: "Fix lawnmower", finished: true },
+                ]
+            },
+        ];
+        assert.strictEqual(
+            serde.serialize(data),
+            "=== Work ===\n- Submit reimbursement form\n- ~Prep for 1:1~\n\n" +
+            "=== School ===\n\n=== Home ===\n- ~Buy milk~\n- ~Fix lawnmower~\n\n"
+        );
+    });
+});
+
+
+describe("deserialize", function () {
+    it("should deserialize empty string to empty list", function () {
+        assert.deepStrictEqual(serde.deserialize(""), []);
+    });
+
+    it("should handle one empty section", function () {
+        assert.deepStrictEqual(
+            serde.deserialize("=== Work ===\n\n"),
+            [{ title: "Work", items: [] }]
+        );
+    });
+
+    it("should handle one section with one unfinished item", function () {
+        assert.deepStrictEqual(
+            serde.deserialize("=== Work ===\n- Finish code review\n\n"),
+            [
+                {
+                    title: "Work",
+                    items: [{ text: "Finish code review", finished: false }]
+                }
+            ]
+        )
+    });
+
+    it("should handle one section with one finished item", function () {
+        assert.deepStrictEqual(
+            serde.deserialize("=== Work ===\n- ~Finish code review~\n\n"),
+            [
+                {
+                    title: "Work",
+                    items: [{ text: "Finish code review", finished: true }]
+                }
+            ]
+        )
+    });
+
+    it("should handle one section with multiple items", function () {
+        assert.deepStrictEqual(
+            serde.deserialize(
+                "=== Work ===\n- ~Finish code review~\n- Polish slides\n\n"
+            ),
+            [
+                {
+                    title: "Work",
+                    items: [
+                        { text: "Finish code review", finished: true },
+                        { text: "Polish slides", finished: false }
+                    ]
+                }
+            ]
+        )
+    });
+
+    it("should handle multiple sections, all with items", function () {
+        assert.deepStrictEqual(
+            serde.deserialize(
+                "=== Work ===\n- Submit form\n- ~Prep for 1:1~\n\n" +
+                "=== Home ===\n- ~Buy milk~\n- ~Fix lawnmower~\n\n"
+            ),
+            [
+                {
+                    title: "Work",
+                    items: [
+                        { text: "Submit form", finished: false },
+                        { text: "Prep for 1:1", finished: true },
+                    ],
+                },
+                {
+                    title: "Home",
+                    items: [
+                        { text: "Buy milk", finished: true },
+                        { text: "Fix lawnmower", finished: true },
+                    ],
+                },
+            ]
+        );
+    });
+
+    it("should handle multiple sections, some empty", function () {
+        assert.deepStrictEqual(
+            serde.deserialize(
+                "=== Work ===\n- Submit form\n- ~Prep for 1:1~\n\n" +
+                "=== School ===\n\n=== Home ===\n- ~Buy milk~\n" +
+                "- ~Fix lawnmower~\n\n"
+            ),
+            [
+                {
+                    title: "Work",
+                    items: [
+                        { text: "Submit form", finished: false },
+                        { text: "Prep for 1:1", finished: true },
+                    ],
+                },
+                {
+                    title: "School",
+                    items: [],
+                },
+                {
+                    title: "Home",
+                    items: [
+                        { text: "Buy milk", finished: true },
+                        { text: "Fix lawnmower", finished: true },
+                    ],
+                },
+            ]
+        );
+    });
+
+    // TODO: Some deserialization tests with unusual whitespace
+});
